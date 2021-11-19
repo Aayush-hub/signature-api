c878b31d6e77e64bbba259e5eaea6426a261cf71
Aayush-hub <ayushgarg1951@gmail.com>
2021-11-19 23:19:19 +0530

    Added app

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# signature-api
+# Signature API
+
--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from cashfree_sdk.payouts import payouts_config_var
 import time
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import time
 import base64
 from cashfree_sdk.exceptions.exceptions import *
@@ -13,12 +13,13 @@ Payouts.init("CF156514C693AFFBVPR34I1OCVPG", "f9894c80185ef723a761e9e0567befdb3f
 
 keyDER = base64.b64decode('MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAy1tQMmHsdFysWCymxntfdc4qhFhL5Jlv/94YPuNgIpbmEySPMsZ1xg04emJE1VtMNVaKpyuz4IFQvwESQC87DqKRmXQd3/eVF1Sdd0IDfKquJb/QXEbUA+xDewTgsjALoex+8IKPUaNeXRYkH+gM21igV4ELtriF+JciMzRdz7QpYo31fqeX9S2poqBV0Uq5pgtJedwuY/cQa8EpT1kSalBFu0f6x5uuDuS7J2kjFyYjm8ahBUyV9tVB+Ye0b5iXM3LsLiKwygeA6JSF7f+PnWVaCnEetIkG8bMG9/1JHJdYsDQLJ8+INX+ucjjswH+GjLZI2CH5s2H06csIc3xFUwIDAQAB')
 keyPub = RSA.importKey(keyDER)
-
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
-    return "Hello World"
+def initial_route():
+    return render_template('index.html')
+
 
 @app.route('/password', methods=['GET'])
 def generate_signature():
@@ -36,8 +37,20 @@ def generate_signature():
     return jsonify(signature_str=signature_str)
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
-print(generate_signature())
+"""
+server
+client -> POST request HIT-> server ->  prediction back to client 
+
+Steps
+---------------
+# Get audio file and save it 
+# Invoke the service (ASR)
+# make a prediction 
+# remove the audio file  from the current dir
+# send back the predicted keywors or string to client in json format
+
+"""
